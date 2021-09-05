cf851d10289bf36d3cc461a48bf2f6737cc4f3f0
Su-nlight <84061534+Su-nlight@users.noreply.github.com>
2021-09-05 19:24:36 +0530

    No3.py

---
+++ No3.py
@@ -0,0 +1,20 @@
+''' Q). Write a program to display area of square and rectangle if user's 
+choice is 1  otherwise if choice is 2 display parameter of both.'''
+
+s=int(input("Enter Side of Square:"))
+l=int(input("Enter Length of Rectangle:"))
+b=int(input("Enter Breadth of Rectangle:"))
+ch=int(input("Enter your Choice:"))
+
+if ch==1:
+  ar1=s**2
+  ar2=l*b
+  print("Area of Square = "+ str(ar1) +" \n Area of Rectangle = " + str(ar2)
+        
+elif ch==2:
+  per1=4*s
+  per2=2*(l+b)
+  print("Perimeter of Square" + str(per1) +"\n Perimeter of Rectangle" + str(per2)
+        
+else:
+  print("Invalid choice")
