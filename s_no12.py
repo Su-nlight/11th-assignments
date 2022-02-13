8caecc22fd535fb2c5d2363c470a7918a08af4ae
Sumit Sharma <84061534+Su-nlight@users.noreply.github.com>
2022-02-13 13:43:58 +0530

    Delete s_no12.py

--- s_no12.py
+++
@@ -1,8 +0,0 @@
-def dupl(x):
-    repeated = []
-    for i in range(len(x)):
-        k=i+1
-        for j in range(k, len(x)):
-            if x[i]==x[j] and x[i] not in repeated:
-                repeated.append(x[i])
-    return repeated
