c0bcc67f9131f535b5a87b9bc92bbcad43ba7bd0
Sumit Sharma <84061534+Su-nlight@users.noreply.github.com>
2022-02-16 14:20:49 +0530

    Update practical_12.py

--- Practicals/practical_12.py
+++ Practicals/practical_12.py
@@ -19,20 +19,16 @@ class students:
         if name1 in self.name: return True
         else: return False
     def add_name(self):
-        for i in range(self.n):
+        i=0
+        while i<self.n:
             newname = input("Enter name of student {}: ".format(i + 1))
             if isempty(newname) == True:
                 print("The name entered is blank so it is not acceptable")
-                i -= 1
-                print(i)
-                continue
             if self.does_exists(name1=newname) == True:
                 print("User already does exist in the tuple")
-                i -= 1
-                print(i)
-                continue
             else:
                 self.name.append(newname)
                 print(newname, "is added to the tuple")
+                i+=1
 
 students()
