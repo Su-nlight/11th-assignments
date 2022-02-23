134874e1a0c4e4f8fae4891a36bf2de07004c03a
Sumit Sharma <84061534+Su-nlight@users.noreply.github.com>
2022-02-23 19:22:20 +0530

    Update practical_11.py

--- Practicals/practical_11.py
+++ Practicals/practical_11.py
@@ -17,6 +17,3 @@ for i in range(n):
     domain.append(emailid[i].split("@")[-1])
 emailid,username,domain=tuple(emailid),tuple(username),tuple(domain)
 print(emailid,username,domain)
-#Write a program to read email IDs of n number of students and store them in a tuple.
-#Create two new tuples, one to store only the usernames from the email IDs and second to store
-#domain names from the email IDs. Print all three tuples at the end of the program.
